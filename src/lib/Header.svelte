c7c9d375df89dbc2f47437fa76c870377bb08c54
Nathan Owens <nowens@ccgideas.com>
2022-02-27 12:15:44 -0600

    basic header styling

---
+++ src/lib/Header.svelte
@@ -0,0 +1,90 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let menuOpen = false;
+
+	const handleClick = () => {
+		menuOpen = !menuOpen;
+	}
+</script>
+
+<header>
+	<div class="container">
+		<div class="logo">
+			<a href="/">Logo</a>
+		</div>
+
+		<nav>
+			<div class="menu-btn {menuOpen ? 'open' : ''}" 
+				on:click={handleClick} 
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			{#if menuOpen}
+				<ul transition:fade>
+					<li>
+						<a href="/">Link 1</a>
+					</li>
+					<li>
+						<a href="/">Link 2</a>
+					</li>
+				</ul>
+			{/if}
+		</nav>
+	</div>
+</header>
+
+<style>
+	header {
+		padding: 30px 0;
+	}
+
+	header .container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.menu-btn {
+		width: 33px;
+		height: 33px;
+		border-radius: 100%;
+		background: var(--mustard);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		cursor: pointer;
+	}
+
+	.menu-btn span {
+		background: white;
+		display: block;
+		margin: 2px;
+		width: 18px;
+		height: 2px;
+		transition: .3s;
+		position: relative;
+		transform: rotate(0);
+	}
+
+	.menu-btn.open span:nth-child(2) {
+		opacity: 0;
+	}
+	.menu-btn.open span:first-child {
+		transform: rotate(45deg);
+		top: 6px;
+	}
+	.menu-btn.open span:last-child {
+		transform: rotate(-45deg);
+		bottom: 5px;
+	}
+
+	nav ul {
+		position: fixed;
+		left: 0;
+		top: 0;
+	}
+	
+</style>

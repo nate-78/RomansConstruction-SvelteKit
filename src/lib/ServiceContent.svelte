a85aecba80e9624dbc0234ee0ee00d18c96c2403
Nathan Owens <nowens@ccgideas.com>
2022-03-26 12:52:29 -0500

    service content responsive edits ;

--- src/lib/ServiceContent.svelte
+++ src/lib/ServiceContent.svelte
@@ -42,9 +42,12 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    display: grid;
+    grid-template-columns: 8fr 4fr;
+    grid-gap: 3rem;
   }
   .left {
-    margin-right: 2rem;
+    /* margin-right: 2rem; */
     position: relative;
   }
   .right {
@@ -63,12 +66,18 @@
     content: '';
     width: 112px;
     height: 112px;
+    width: 18%;
+    height: unset;
+    aspect-ratio: 1;
     background: var(--mustard);
   }
   .main-img img {
     width: 582px;
     margin-left: 150px;
     margin-bottom: 65px;
+    width: 75%;
+    margin-left: 25%;
+    margin-bottom: 11%;
     position: relative;
   }
   .small-img {
@@ -76,11 +85,16 @@
     bottom: 0;
     left: 0;
     width: 225px;
+    width: 31%;
+    aspect-ratio: 1;
   }
   .small-img::before {
     content: '';
     width: 112px;
     height: 112px;
+    width: 52%;
+    height: unset;
+    aspect-ratio: 1;
     background: var(--light-blue);
     left: -9px;
     bottom: -3px;
@@ -88,5 +102,50 @@
   }
   .small-img img {
     position: relative;
+  }
+
+  @media (max-width: 1000px) {
+    section {
+      padding-top: 10px;
+    }
+  }
+
+  @media (min-width: 767px) and (max-width: 900px) {
+    .small-img {
+      display: none;
+    }
+    .main-img img {
+      margin: 0;
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 767px) {
+    .row {
+      display: flex;
+      flex-direction: column-reverse;
+      grid-gap: 1rem;
+    }
+    .right {
+      min-width: 550px;
+      max-width: 550px;
+    }
+  }
+
+  @media (max-width: 630px) {
+    .right {
+      min-width: 100%;
+      max-width: 100%;
+    }
+  }
+
+  @media (max-width: 400px) {
+    .small-img {
+      display: none;
+    }
+    .main-img img {
+      margin: 0;
+      width: 100%;
+    }
   }
 </style>
